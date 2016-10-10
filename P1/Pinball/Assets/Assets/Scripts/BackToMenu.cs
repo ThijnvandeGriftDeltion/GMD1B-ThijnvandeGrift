c600eaf00084ccdd9398b0c87d5efa36c900891c
ThijnvandeGriftDeltion <97044184@st.deltion.nl>
2016-10-10 10:59:11 +0200

    Unity project update

---
+++ P1/Pinball/Assets/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToMenu : MonoBehaviour {
+	
+	public Canvas menu;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Back ();
+		CanvasUit ();
+	}
+	
+	public void Back () {
+		if(Input.GetButtonDown ("Jump")) {
+			menu.enabled = true;
+		}
+	}
+	
+	public void CanvasUit () {
+		menu.enabled = false;
+	}
+}

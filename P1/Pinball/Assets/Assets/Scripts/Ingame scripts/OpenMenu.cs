5b0a5a884b859c596a522f8a6006b07b33dcab48
ThijnvandeGriftDeltion <97044184@st.deltion.nl>
2016-10-11 15:35:52 +0200

    Unity project
    Ingame menu

---
+++ P1/Pinball/Assets/Assets/Scripts/Ingame scripts/OpenMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMenu : MonoBehaviour {
+	
+	public GameObject menu;
+	public GameObject controls;
+	public bool menuaan;
+
+	void Update () {
+		Menuaan();
+		ControlsUit();
+	}
+	
+	public void Menuaan () {
+		if (Input.GetButtonDown("Escape")) {
+			menu.SetActive(true);
+		}
+	}
+
+	public void Resume() {
+		menu.SetActive(false);
+	}
+		
+	public void Controls() {
+		menu.SetActive(false);
+		controls.SetActive(true);
+	}
+	
+	public void ControlsUit() {
+		if (Input.GetButtonDown("Escape")) {
+			controls.SetActive(false);
+			menu.SetActive(true);
+		}
+	}
+	
+	public void LoadScene(int level) {
+		Application.LoadLevel(level);
+	}
+	
+	public void ExitGame() {
+		Application.Quit();
+	}
+}

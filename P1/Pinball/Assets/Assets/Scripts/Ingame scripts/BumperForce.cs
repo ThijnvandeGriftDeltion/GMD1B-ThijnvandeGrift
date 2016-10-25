6cd4538410c90ef3158a1fd997b5ab640a85d23f
ThijnvandeGriftDeltion <97044184@st.deltion.nl>
2016-10-25 19:37:12 +0200

    Unity Pinball project
    Meer objecten in het veld, bumbers bouncen de bal weg en belichting is
    iets anders.

---
+++ P1/Pinball/Assets/Assets/Scripts/Ingame scripts/BumperForce.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumperForce : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody pinball;
+	public GameObject ball;
+	
+	void Start () {
+		pinball = ball.GetComponent<Rigidbody>();
+	}
+	
+	public void OnCollisionEnter (Collision collision) {
+		pinball.AddForce(5,5,5 * thrust);
+	}
+}
